68013681d41f31510959f43dfbae9cdffd5f6a25
ABisht <ABisht@DESKTOP-RGGE3Q3>
2020-05-22 16:41:09 +0530

    WebClient implements

--- movie-catalog-service/src/main/java/io/abisht/moviecatalogservice/controller/MovieController.java
+++ movie-catalog-service/src/main/java/io/abisht/moviecatalogservice/controller/MovieController.java
@@ -5,11 +5,13 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import io.abisht.moviecatalogservice.models.CatalogItem;
 import io.abisht.moviecatalogservice.models.Movie;
@@ -19,7 +21,11 @@ import io.abisht.moviecatalogservice.models.Rate;
 @RequestMapping("/catalog")
 public class MovieController {
 
-	RestTemplate resttemp=new RestTemplate();
+	@Autowired
+	private RestTemplate resttemp;
+	
+	@Autowired
+	private WebClient.Builder webClient;
 	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId")String userId){
@@ -30,10 +36,28 @@ public class MovieController {
 				);
 		
 		return rating.stream().map(rate->{	
-			Movie movie=resttemp.getForObject("http://localhost:8081/movie/"+rate.getMovieId(), Movie.class);
+			//Movie movie=resttemp.getForObject("http://localhost:8081/movie/"+rate.getMovieId(), Movie.class);
+			
+			Movie movie=webClient.build()
+			.get()
+			.uri("http://localhost:8081/movie/"+rate.getMovieId())
+			.retrieve()
+			.bodyToMono(Movie.class)
+			.block();
 			return new CatalogItem(movie.getName(), "Test",rate.getRating());
 			
 		}).collect(Collectors.toList());
+		
+
+		/*
+		 * return rating.stream().map(rate->{ Movie
+		 * movie=resttemp.getForObject("http://localhost:8081/movie/"+rate.getMovieId(),
+		 * Movie.class); return new CatalogItem(movie.getName(),
+		 * "Test",rate.getRating());
+		 * 
+		 * }).collect(Collectors.toList());
+		 */	
 		}
+	
 	
 }
